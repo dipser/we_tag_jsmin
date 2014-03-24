79b4256a7eac7ac2c83eef052dc12a12d6b27629
Aurel Hermand <github@devone.de>
2014-03-24 15:16:05 +0100

    Kopieren der Dateien

---
+++ copy.php
@@ -0,0 +1,8 @@
+<?php
+
+copy('webEdition/we/include/we_tags/custom_tags/JSMin.php', '../webEdition/we/include/we_tags/custom_tags/JSMin.php');
+copy('webEdition/we/include/we_tags/custom_tags/we_tag_jsmin.inc.php', '../webEdition/we/include/we_tags/custom_tags/we_tag_jsmin.inc.php');
+
+copy('webEdition/we/include/weTagWizard/we_tags/custom_tags/we_tag_jsmin.inc.php', '../webEdition/we/include/weTagWizard/we_tags/custom_tags/we_tag_jsmin.inc.php');
+
+?>
